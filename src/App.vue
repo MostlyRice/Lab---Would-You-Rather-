255331238ed274209594f44926eaf860cc250613
MostlyRice <amazinzygy@gmail.com>
2022-11-07 10:02:56 -0600

    formatted

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-<div id="app">
-  <h1>Would you rather...</h1>
-    <would-you-rather></would-you-rather>
-</div>
+  <!-- template tag needs to have exactly one child element -->
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <h1>Would you rather?</h1>
+    <!-- you can use the uppercase style, but the dashes between is the convention for writing vue tagnames
+         There is no valid HTML tag with dashes-->
+    <!-- TODO v-for to loop over the list of questions & answers -->
+    <would-you-rather
+      v-for="(question) in questionsArray"
+      :key="question.id"
+      v-bind:question="question.wyrQuestion"
+      v-bind:answer1="question.answer1"
+      v-bind:answer2="question.answer2"
+      v-bind:id="question.id"
+      v-on:answer-changed="answerChanged"
+    >
+    </would-you-rather>
+    <!-- 'app' is the main page, WouldYouRather is inside it,
+         we need to specifiy that app should show this component-->
+    <!-- create unordered list of selections -->
+    <p id="ywr">You Would Rather...</p>
+    <!-- loop over userSelectionsArray, put each response in an unordered list. -->
+    <ul id="userSelectionsArray">
+      <li v-for="(selection) in userSelectionsArray">
+        {{ selection }}
+      </li>
+    </ul>
+  </div>
 </template>
-
 <script>
 import WouldYouRather from './components/WouldYouRather.vue'
 
@@ -13,10 +35,44 @@ export default {
   name: 'App',
   components: {
     WouldYouRather
+  },
+  // Create a questions array to hold all the questions/possible answers
+  data() {
+    return {
+      questionsArray: [
+        {
+          id: 0,
+          question: 'Would you rather team up with Wonder Woman or Captain Marvel',
+          answer1: 'Wonder Woman',
+          answer2: 'Captain Marvel',
+        },
+        {
+          id: 1,
+          question:'Would you rather find true love today or win the lottery next year?',
+          answer1: 'Find true love',
+          answer2: 'Win the lottery',
+        },
+        {
+          id: 2,
+          question: 'Would you rather have another 10 years with your partner or a one-night stand with your celebrity crush?',
+          answer1: '10 years with my partner',
+          answer2: 'One night stand with my celeberity crush',
+        }
+      ],
+      userSelectionsArray: [
+      ],
+    }
+  },
+  methods: {
+    // use question IDs to update user choices-- insert them at correct index (which matches the id#)
+    // replace the old response when a new choice is made for each question
+      answerChanged(choice, id) {
+        this.userSelectionsArray.splice(id, 1, choice)
+      }
+
   }
 }
 </script>
-
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -25,5 +81,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  background: thistle;
+}
+
+#ywr {
+  background: mediumseagreen;
 }
 </style>
--- src/components/WouldYouRather.vue
+++ src/components/WouldYouRather.vue
@@ -1,32 +1,63 @@
 <template>
-  <div class="hello">
-    <h2>Please make your choice!</h2>
-  </div>
-</template>
-
-<script>
-export default {
-  name: 'WouldYouRather',
-  props: {
-    msg: String
-  }
-}
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+   <div class="hello">
+     <h2>Please make your choice!</h2>
+     <h3>{{ question }}</h3>
+     <div>
+       <input
+         type="radio"
+         v-model="choice"
+         v-bind:value="answer1"
+         v-on:change="choiceMade"
+       />
+       <label>{{ answer1 }}</label>
+     </div>
+     <div>
+       <input
+         type="radio"
+         v-model="choice"
+         v-bind:value="answer2"
+         v-on:change="choiceMade"
+       />
+       <label>{{ answer2 }}</label>
+     </div>
+   </div>
+ </template>
+ <script>
+ export default {
+   name: 'WouldYouRather',
+   // these are the properties that can be passed to the child component
+   props: {
+     question: String,
+     answer1: String,
+     answer2: String,
+     id: Number,
+   },
+   data() {
+     return {
+       choice: ''
+     }
+   },
+   methods: {
+     choiceMade() {
+       this.$emit('answer-changed', this.choice, this.id) // pass choice & id for tracking over to app.vue
+     }
+   }
+ }
+ </script>
+ <style scoped>
+ h3 {
+   margin: 40px 0 0;
+ }
+ ul {
+   list-style-type: none;
+   padding: 0;
+ }
+ li {
+   display: inline-block;
+   margin: 0 10px;
+ }
+ a {
+   color: #42b983;
+ }
+ </style>
+ 
