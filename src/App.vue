4f4f61c5b1f8cf8a7b04b803e011c2a9a41fa595
MostlyRice <amazinzygy@gmail.com>
2022-11-07 10:03:52 -0600

    added body color

--- src/App.vue
+++ src/App.vue
@@ -74,6 +74,11 @@ export default {
 }
 </script>
 <style>
+
+body {
+  background: thistle;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -81,7 +86,7 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-  background: thistle;
+
 }
 
 #ywr {
